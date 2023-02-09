29985f383360aa3a4e81d5f7b66d9df88add4d71
bbenligiray <bbenligiray@gmail.com>
2023-02-09 12:23:24 +0300

    Also verify the deployed implementation on Etherscan

--- deploy/2_verify.js
+++ deploy/2_verify.js
@@ -7,5 +7,15 @@ module.exports = async ({ deployments }) => {
     address: StakeableVestingFactory.address,
     constructorArguments: [references.Api3Token, references.Api3Pool],
   });
+  const stakeableVestingFactory = new hre.ethers.Contract(
+    StakeableVestingFactory.address,
+    StakeableVestingFactory.abi,
+    hre.ethers.provider
+  );
+  const stakeableVestingImplementationAddress = await stakeableVestingFactory.stakeableVestingImplementation();
+  await hre.run('verify:verify', {
+    address: stakeableVestingImplementationAddress,
+    constructorArguments: [references.Api3Token, references.Api3Pool],
+  });
 };
 module.exports.tags = ['verify'];
